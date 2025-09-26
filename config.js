81fc0c173bb059cab0137a3d0e71c6e85b160ebb
𝙼𝚛. 𝙰𝚍𝚖𝚒𝚗 <183306473+mister-admin@users.noreply.github.com>
2025-09-26 15:18:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* Central config — меняй здесь версию и другие константы */
-const APP_VERSION = 'v1.0.2 beta'; // <-- изменить здесь — и это обновит футер + i18n version
+const APP_VERSION = 'v1.1.0 beta'; // <-- изменить здесь — и это обновит футер + i18n version
