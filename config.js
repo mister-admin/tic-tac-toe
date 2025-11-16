db85412672ba8c1eb8f1ecc4d167bd82af07d32f
𝙼𝚛. 𝙰𝚍𝚖𝚒𝚗 <183306473+mister-admin@users.noreply.github.com>
2025-11-16 20:17:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Single change point for version and some defaults
-const APP_VERSION = 'v1.1.1 beta'; // <-- поменяй тут версию в один клик
+const APP_VERSION = 'v1.1.2 beta'; // <-- поменяй тут версию в один клик
